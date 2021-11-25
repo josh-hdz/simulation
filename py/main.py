8814cd6ffa3d216a94aff21592530f515a156e54
ariclavel <a01706629@itesm.mx>
2021-11-25 00:02:39 -0400

    cambio

--- py/main.py
+++ py/main.py
@@ -10,17 +10,13 @@ def main():
         },
         'traffic lights': {
             'amount': 2,
-            'time red': 45,
-            'time yellow': 5,
-            'time green': 50
+            'time red': 5,
+            'time yellow': 2,
+            'time green': 5
         },
-<<<<<<< HEAD
         'a': 400,
         'step time': 0.01,
-=======
-        'a': 500,
-        'step time': 0.1,
->>>>>>> fabe1700162b8c9992747726300458f36d19eb8c
+
         'b': 5,
         'l': 3.6,
         "density": 0.5,
